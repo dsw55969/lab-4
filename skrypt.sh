10cdf91129d14ad791be4f67567b4f9df285df6b
Wiktoria <dsw55969@dsw.edu.pl>
2025-05-17 21:58:14 +0200

    log

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,13 @@
 if [[ "$1" == "--date" ]]; then
   date
 fi
+
+elif [[ "$1" == "--logs" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log${i}.txt"
+    echo "Plik: $filename" > $filename
+    echo "Skrypt: $0" >> $filename
+    echo "Data: $(date)" >> $filename
+  done
+fi
